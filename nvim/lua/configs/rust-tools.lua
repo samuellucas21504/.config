4ddab159c5675273c63b11cc97c61a7dd14eea01
Samuel Lucas <samuellucasrdg@gmail.com>
2024-07-08 20:02:39 +0000

    added nvim

---
+++ nvim/lua/configs/rust-tools.lua
@@ -0,0 +1,12 @@
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local options = {
+  server = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+}
+
+return options
---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,24 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- Map <leader>db to add a breakpoint rust
+map('n', '<leader>db', '<cmd> DapToggleBreakpoint <CR>', { noremap = true, silent = true })
+
+-- Map 't' to open terminal 
+vim.api.nvim_set_keymap('n', 't', ':split | term<CR>', { noremap = true, silent = true })
+
+--Code runner
+
+map('n', '<leader>r', ':RunCode<CR>', { noremap = true, silent = false })
+map('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
+map('n', '<leader>rft', ':RunFile tab<CR>', { noremap = true, silent = false })
+map('n', '<leader>rp', ':RunProject<CR>', { noremap = true, silent = false })
+map('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false })
+map('n', '<leader>crf', ':CRFiletype<CR>', { noremap = true, silent = false })
+map('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
